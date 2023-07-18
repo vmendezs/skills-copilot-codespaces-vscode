b9555558bd26a9f6160fa6f906e982e9e3c1a23a
Valeria Méndez <56889089+vmendezs@users.noreply.github.com>
2023-07-18 17:13:07 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -2,6 +2,9 @@ function skillsMember() {
   return {
     restrict: 'E',
     templateUrl: 'templates/skills-member.html',
+    controller: 'SkillsMemberController',
+    controllerAs: 'vm',
+    bindToController: true,
     scope: {
       member: '='
     }
